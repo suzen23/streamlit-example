4409cdf54141cf2c6d65da409d87160457f38a77
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 22:24:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
-from collections import namedtuple
-import altair as alt
-import math
+"""from collections import namedtuple
+#import altair as alt
+#import math
+#import pandas as pd
+#import streamlit as st """
+
 import pandas as pd
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
 # Welcome to Streamlit!
@@ -16,7 +21,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
+""" with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
@@ -35,4 +40,4 @@ with st.echo(code_location='below'):
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        .encode(x='x:Q', y='y:Q'))  """
