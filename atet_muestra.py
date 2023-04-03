b3a77a8f1c9704cc980952c62e75787e6289963f
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 23:11:15 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -3,3 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.header=('DESESTIMIENTOS ATET')
+
+basepath ="https://github.com/suzen23/streamlit-example"
+archivo1= basepath + "/ListDesestimiento.xlsx"
+
+
+dfind = pd.read_excel(archivo1, sheet_name="LISTADO")
+dexo = pd.read_excel(archivo1, sheet_name="Motivo")
+
+dfind.head()
