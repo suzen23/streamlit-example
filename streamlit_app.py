d0bfdf51a2ede7f53cc9ed4635077fa7cf4c18d6
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 22:25:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-""" with st.echo(code_location='below'):
+ with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
@@ -40,4 +40,4 @@ In the meantime, below is an example of what you can do with just a few lines of
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))  """
+        .encode(x='x:Q', y='y:Q'))  
