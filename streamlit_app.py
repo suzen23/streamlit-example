3fbd2725b7c2fe5194635eba88de664000a94c95
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 23:42:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,11 @@ import numpy as np
 
 st.header('DESESTIMIENTOS DE ATET')
 
-st.header=('DESESTIMIENTOS ATET')
-st.subheader=('des')
-
-#cargar el dataframe
-#basepath ="https://github.com/suzen23/streamlit-example"
-#archivo1= basepath + "/ListDesestimientoCopia.xlsx"
+st.subheader('holas')
 
 excel_file = 'ListDesestimiento.xlsx'
 sheet_name ='DATA'
+
 df =pd.read_excel(excel_file,
                   sheet_name = sheet_name,
                   usecols='B:D',
