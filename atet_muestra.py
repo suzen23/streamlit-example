ae94faac721f27f0441cc09a28e37136df5d35ce
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 23:31:35 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 st.header=('DESESTIMIENTOS ATET')
 
 #cargar el dataframe
+#basepath ="https://github.com/suzen23/streamlit-example"
+#archivo1= basepath + "/ListDesestimientoCopia.xlsx"
 
-excel_file = 'ListDesestimiento.xlsx'
+excel_file = 'https://github.com/suzen23/streamlit-example/ListDesestimiento.xlsx'
 sheet_name ='DATA'
 df =pd.read_excel(excel_file,
                   sheet_name = sheet_name,
