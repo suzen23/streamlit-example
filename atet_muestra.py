b38ab2dc76478fb4fb019faa2df81a2a6993e5df
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 23:24:51 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -6,7 +6,7 @@ st.header=('DESESTIMIENTOS ATET')
 
 #cargar el dataframe
 
-excel_file = 'ListDesestimiento.xlsx
+excel_file = 'ListDesestimiento.xlsx'
 sheet_name ='DATA'
 df =pd.read_excel(excel_file,
                   sheet_name = sheet_name,
