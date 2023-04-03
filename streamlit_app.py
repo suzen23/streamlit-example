c36e8ffcd44b394aeb4abf7d6b9b15ae71ad00bc
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 22:33:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,15 @@
-"""from collections import namedtuple
-#import altair as alt
-#import math
-#import pandas as pd
-#import streamlit as st """
-
-import pandas as pd
-import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
+from collections import namedtuple
+import altair as alt
 import math
+import pandas as pd
+import streamlit as st 
 
-# Welcome to Streamlit!
 
-#Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+ Welcome to Streamlit!
 
-"""SI TU you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+SI TU you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
@@ -39,4 +34,4 @@ In the meantime, below is an example of what you can do with just a few lines of
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))  """
+        .encode(x='x:Q', y='y:Q'))  
